721ae6b643b8ad4d33f76a2cc22d33e47bb5791d
Massimo Cibeira Occhiuzzi <cibeiramassimo@gmail.com>
2024-09-01 00:12:07 -0300

    pdf guia de ejercicios y ejercicio 1

---
+++ ejercicio1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<< "Numero uno: " <<endl;
+    cin>>x;
+    cout<< "Numero dos: " <<endl;
+    cin>>y;
+
+    cout<< "Suma = " << x + y <<endl;
+    cout<< "Resta = " << x - y <<endl;
+    cout<< "Producto = " << x * y <<endl;
+}
